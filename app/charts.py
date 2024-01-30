86db9cfce98d2e1bd0201f51cf599b26a117b2f9
Drokem <moisescosme01@gmail.com>
2024-01-30 16:03:40 -0300

    cargando progreso

--- app/charts.py
+++ app/charts.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart():
-  labels = ['a', 'b', 'c']
-  values = [10, 200, 80]
-  
-  fig, ax = plt.subplot()
+def generate_bar_chart(name, labels, values):
+  fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.savefig(f'./imgs/{name}.png')
+  plt.close()
+   
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.savefig('pie.png')
+  plt.close()
 
 if __name__ == '__main__':
-  generate_bar_chart()
+  labels = ['a', 'b', 'c']
+  values = [10, 40, 800]
+  # generate_bar_chart(labels, values)
+  generate_pie_chart(labels, values)
--- app/main.py
+++ app/main.py
@@ -1,24 +1,25 @@
 import utils
-
-data = [
-  {
-    'Country': 'Colombia',
-    'population': 500
-  },
-  {
-    'Country': 'Bolivia',
-    'population': 300
-  }
-]
+import read_csv
+import charts
 
 def run():
-  keys, values = utils.get_polulation()
-  print(keys, values)
-  
+  data = read_csv.read_csv('data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
   country = input('Type Country => ')
-  
+  print(country)
+
   result = utils.population_by_country(data, country)
-  print(result)
+  
+  if len(result) > 0:
+    country = result[0]
+    print(country)
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
 
 if __name__ == '__main__':
-run()
+  run()
